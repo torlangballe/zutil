71cff0f4c704b9d9423db875fa60aaa81d87fca7
Tor Langballe <tor.langballe@bridgetech.tv>
2022-03-26 11:44:56 +0100

    Move zui.KeyValueStore to zutil/zkeyvalue

---
+++ zkeyvalue/KeyValueStore.go
@@ -0,0 +1,121 @@
+package zkeyvalue
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/torlangballe/zutil/zdict"
+	"github.com/torlangballe/zutil/zlog"
+)
+
+//  Created by Tor Langballe on /30/10/15.
+
+// For storage:
+// https://github.com/peterbourgon/diskv
+// https://github.com/recoilme/pudge
+// https://github.com/nanobox-io/golang-scribble
+
+type Store struct {
+	Local     bool   // if true, only for single browser or device, otherwise for user anywhere
+	Secure    bool   // true if key/value stored in secure key chain
+	KeyPrefix string // this can be a user id. Not used if key starts with /
+	filepath  string // Some variants of store use this
+}
+
+var DefaultStore *Store
+
+func NewStore(local bool) *Store {
+	return &Store{Local: local}
+}
+
+func (s Store) GetObject(key string, objectPtr interface{}) (got bool) {
+	var rawjson string
+	got = s.getItem(key, &rawjson)
+	if got {
+		err := json.Unmarshal([]byte(rawjson), objectPtr)
+		if zlog.OnError(err, "unmarshal") {
+			return
+		}
+	}
+	return
+}
+
+func (s Store) GetString(key string) (str string, got bool) {
+	got = s.getItem(key, &str)
+	return
+}
+
+func (s Store) GetDict(key string) (dict zdict.Dict, got bool) {
+	got = s.GetObject(key, &dict)
+	return
+}
+
+func (s Store) GetInt64(key string, def int64) (val int64, got bool) {
+	got = s.getItem(key, &val)
+	if got {
+		return
+	}
+	return def, true
+}
+
+func (s Store) GetInt(key string, def int) (int, bool) {
+	n, got := s.GetInt64(key, int64(def))
+	// zlog.Info("KVS GetInt:", key, n, got)
+	return int(n), got
+}
+
+func (s Store) GetDouble(key string, def float64) (val float64, got bool) {
+	got = s.getItem(key, &val)
+	if got {
+		return val, true
+	}
+	return def, true
+}
+
+func (s Store) GetTime(key string) (time.Time, bool) {
+	return time.Time{}, false
+}
+
+func (s Store) GetBool(key string, def bool) (val bool, got bool) {
+	got = s.getItem(key, &val)
+	if got {
+		return val, true
+	}
+	return def, true
+}
+
+func (s Store) IncrementInt(key string, sync bool, inc int) int {
+	return 0
+}
+
+func (s Store) RemoveForKey(key string, sync bool) {
+
+}
+
+func (s Store) SetObject(object interface{}, key string, sync bool) {
+	data, err := json.Marshal(object)
+	if zlog.OnError(err, "marshal") {
+		return
+	}
+	s.setItem(key, string(data), sync)
+}
+func (s Store) SetString(value string, key string, sync bool)  { s.setItem(key, value, sync) }
+func (s Store) SetDict(dict zdict.Dict, key string, sync bool) { s.SetObject(dict, key, sync) }
+func (s Store) SetInt64(value int64, key string, sync bool)    { s.setItem(key, value, sync) }
+func (s Store) SetInt(value int, key string, sync bool) {
+	// zlog.Info("KVS: SetInt:", value, key)
+	s.setItem(key, value, sync)
+}
+func (s Store) SetDouble(value float64, key string, sync bool) { s.setItem(key, value, sync) }
+func (s Store) SetBool(value bool, key string, sync bool)      { s.setItem(key, value, sync) }
+func (s Store) SetTime(value time.Time, key string, sync bool) { s.setItem(key, value, sync) }
+func (s Store) ForAllKeys(got func(key string))                {}
+
+func (s Store) prefixKey(key *string) {
+	if (*key)[0] != '/' && s.KeyPrefix != "" {
+		*key = s.KeyPrefix + "/" + *key
+	}
+	if zlog.IsInTests {
+		*key += "_test"
+	}
+}
