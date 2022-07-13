19e1db28eaf59b8862bb22b684ce6b41b1287873
Tor Langballe <tor.langballe@bridgetech.tv>
2022-07-13 13:47:02 +0200

    Add GetIP4Address methods

--- znet/znet.go
+++ znet/znet.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"fmt"
 	"net"
 	"net/url"
 	"runtime"
@@ -87,5 +88,36 @@ func GetCurrentLocalIPAddress() (ip16, ip4 string, err error) {
 			}
 		}
 	}
+	return
+}
+
+func GetIP4AddressAsParts(address string) (parts []int, err error) {
+	sparts := strings.Split(address, ".")
+	if len(sparts) != 4 {
+		return nil, zlog.NewError("wrong number of parts:", len(sparts), address)
+	}
+	for _, p := range sparts {
+		n, err := strconv.Atoi(p)
+		if err != nil {
+			return nil, err
+		}
+		if n < 0 || n > 255 {
+			return nil, zlog.NewError("number out of range:", n)
+		}
+		parts = append(parts, n)
+	}
+	return
+}
+
+func GetIP4PartsAsAddress(parts []int) (address string, err error) {
+	if len(parts) != 4 {
+		return "", zlog.NewError("wrong number of parts:", len(parts))
+	}
+	for _, n := range parts {
+		if n < 0 || n > 255 {
+			return "", zlog.NewError("number out of range:", n)
+		}
+	}
+	address = fmt.Sprintf("%d.%d.%d.%d", parts[0], parts[1], parts[2], parts[3])
 	return
 }
