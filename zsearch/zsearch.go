fa585ab55edbe2fe37d9ae204de7354cf3dcba99
Tor Langballe <tor.langballe@bridgetech.tv>
2022-11-10 13:32:36 +0100

    New package for web-search

---
+++ zsearch/zsearch.go
@@ -0,0 +1,4 @@
+package zsearch
+
+// curl "https://www.googleapis.com/customsearch/v1?key=AIzaSyBrj3rJwnYz0OsWamNDyOt80_TWcUPEPbI&cx=70a15aaff3df44c77&q=dog"
+// https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list#request
