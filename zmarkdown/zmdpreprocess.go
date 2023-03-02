60969c246d155acb0d7e98f5191f7fd66e8280e2
Tor Langballe <tor.langballe@bridgetech.tv>
2023-03-02 15:24:05 +0100

    Skip latex+graphviz dot output in zmarkdown
    So we can cross compile. Will find solution for this later.

--- zmarkdown/zmdpreprocess.go
+++ zmarkdown/zmdpreprocess.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"text/template"
 
-	"github.com/go-latex/latex/drawtex/drawimg"
-	"github.com/go-latex/latex/mtex"
-	"github.com/goccy/go-graphviz"
+	// "github.com/go-latex/latex/drawtex/drawimg"
+	// "github.com/go-latex/latex/mtex"
+	// "github.com/goccy/go-graphviz"
 	"github.com/gorilla/mux"
 	"github.com/torlangballe/zutil/zdict"
 	"github.com/torlangballe/zutil/zfilecache"
@@ -40,48 +40,48 @@ func errToStr(err error, title, desc string) string {
 	return fmt.Sprint(title, "@", desc, ": ", err)
 }
 
-func (t *Templater) processTex(fontScale float64, title, teXStr string) (output string) {
-	zlog.Info("processTex:", title, fontScale, "\n", teXStr)
-	cacheID := getHash(teXStr, TexType, fontScale)
-	if !Cache.IsCached(cacheID) {
-		var buf bytes.Buffer
-		// reader, writer := io.Pipe()
-		dest := drawimg.NewRenderer(&buf)
-		err := mtex.Render(dest, teXStr, t.TeXFontSize*fontScale, float64(t.DPI), nil)
-		if err != nil {
-			return errToStr(err, title, "render")
-		}
-		_, err = Cache.CacheFromData(buf.Bytes(), cacheID)
-		if err != nil {
-			return errToStr(err, title, "get-from-cache")
-		}
-	}
-	return getImageMarkdownFromCacheID(title, cacheID)
-}
+// func (t *Templater) processTex(fontScale float64, title, teXStr string) (output string) {
+// 	zlog.Info("processTex:", title, fontScale, "\n", teXStr)
+// 	cacheID := getHash(teXStr, TexType, fontScale)
+// 	if !Cache.IsCached(cacheID) {
+// 		var buf bytes.Buffer
+// 		// reader, writer := io.Pipe()
+// 		dest := drawimg.NewRenderer(&buf)
+// 		err := mtex.Render(dest, teXStr, t.TeXFontSize*fontScale, float64(t.DPI), nil)
+// 		if err != nil {
+// 			return errToStr(err, title, "render")
+// 		}
+// 		_, err = Cache.CacheFromData(buf.Bytes(), cacheID)
+// 		if err != nil {
+// 			return errToStr(err, title, "get-from-cache")
+// 		}
+// 	}
+// 	return getImageMarkdownFromCacheID(title, cacheID)
+// }
 
-func (t *Templater) processDot(scale float64, title, dotStr string) string {
-	// https://renenyffenegger.ch/notes/tools/Graphviz/examples/index
-	zlog.Info("processDot:", title, scale, "\n", dotStr)
-	cacheID := getHash(dotStr, DotType, scale)
-	if !Cache.IsCached(cacheID) {
-		graph, err := graphviz.ParseBytes([]byte(dotStr))
-		if err != nil {
-			return errToStr(err, title, "parse-bytes")
-		}
-		graph = graph.SetDPI(96 * scale) // can't get SetScale to work, or any attributes in dot graph, so hacking dpi, which is default 96
-		var buf bytes.Buffer
-		g := graphviz.New()
-		err = g.Render(graph, graphviz.PNG, &buf)
-		if err != nil {
-			return errToStr(err, title, "render")
-		}
-		_, err = Cache.CacheFromData(buf.Bytes(), cacheID)
-		if err != nil {
-			return errToStr(err, title, "get-from-cache")
-		}
-	}
-	return getImageMarkdownFromCacheID(title, cacheID)
-}
+// func (t *Templater) processDot(scale float64, title, dotStr string) string {
+// 	// https://renenyffenegger.ch/notes/tools/Graphviz/examples/index
+// 	zlog.Info("processDot:", title, scale, "\n", dotStr)
+// 	cacheID := getHash(dotStr, DotType, scale)
+// 	if !Cache.IsCached(cacheID) {
+// 		graph, err := graphviz.ParseBytes([]byte(dotStr))
+// 		if err != nil {
+// 			return errToStr(err, title, "parse-bytes")
+// 		}
+// 		graph = graph.SetDPI(96 * scale) // can't get SetScale to work, or any attributes in dot graph, so hacking dpi, which is default 96
+// 		var buf bytes.Buffer
+// 		g := graphviz.New()
+// 		err = g.Render(graph, graphviz.PNG, &buf)
+// 		if err != nil {
+// 			return errToStr(err, title, "render")
+// 		}
+// 		_, err = Cache.CacheFromData(buf.Bytes(), cacheID)
+// 		if err != nil {
+// 			return errToStr(err, title, "get-from-cache")
+// 		}
+// 	}
+// 	return getImageMarkdownFromCacheID(title, cacheID)
+// }
 
 func getImageMarkdownFromCacheID(title, cacheID string) string {
 	surl := Cache.GetURLForName(cacheID)
@@ -92,8 +92,8 @@ func (t *Templater) Preprocess(title, markdownText string, variables zdict.Dict)
 	zlog.Assert(Cache != nil, "cache")
 	var buf bytes.Buffer
 	funcMap := template.FuncMap{
-		"tex": t.processTex,
-		"dot": t.processDot,
+		// "tex": t.processTex,
+		// "dot": t.processDot,
 	}
 
 	template, err := template.New(title).Funcs(funcMap).Parse(markdownText)
