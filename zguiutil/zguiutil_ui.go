76709038bd799e7df0e25f0be4ed6f20714abde0
Tor Langballe <tor.langballe@bridgetech.tv>
2023-04-03 09:26:41 +0200

    Move Labelize into zguiutil from zlabel

--- zguiutil/zguiutil_ui.go
+++ zguiutil/zguiutil_ui.go
@@ -4,9 +4,11 @@ package zguiutil
 
 import (
 	"github.com/torlangballe/zui/zcanvas"
+	"github.com/torlangballe/zui/zcheckbox"
 	"github.com/torlangballe/zui/zcontainer"
 	"github.com/torlangballe/zui/zlabel"
 	"github.com/torlangballe/zui/zstyle"
+	"github.com/torlangballe/zui/ztextinfo"
 	"github.com/torlangballe/zui/zview"
 	"github.com/torlangballe/zutil/zgeo"
 	"github.com/torlangballe/zutil/zlog"
@@ -39,4 +41,37 @@ func NewBar(title string) *zcontainer.StackView {
 		canvas.DrawGradient(path, colors, rect.Min(), rect.BottomLeft(), nil)
 	})
 	return bar
+}
+
+func Labelize(view zview.View, prefix string, minWidth float64, alignment zgeo.Alignment) (label *zlabel.Label, stack *zcontainer.StackView, viewCell *zcontainer.Cell) {
+	font := zgeo.FontNice(zgeo.FontDefaultSize, zgeo.FontStyleBold)
+	to, _ := view.(ztextinfo.Owner)
+	if to != nil {
+		ti := to.GetTextInfo()
+		font = ti.Font
+		font.Style = zgeo.FontStyleBold
+	}
+	title := prefix
+	checkBox, isCheck := view.(*zcheckbox.CheckBox)
+	if checkBox != nil && alignment&zgeo.Right != 0 {
+		title = ""
+		_, cstack := zcheckbox.Labelize(checkBox, prefix)
+		view = cstack
+		alignment = alignment.FlippedHorizontal()
+	}
+	label = zlabel.New(title)
+	label.SetObjectName("$labelize.label " + prefix)
+	label.SetTextAlignment(zgeo.Right)
+	label.SetFont(font)
+	label.SetColor(zstyle.DefaultFGColor().WithOpacity(0.7))
+	stack = zcontainer.StackViewHor("$labelize." + prefix) // give it special name so not easy to mis-search for in recursive search
+
+	stack.Add(label, zgeo.CenterLeft).MinSize.W = minWidth
+	marg := zgeo.Size{}
+	if isCheck {
+		marg.W = -6 // in html cell has a box around it of 20 pixels
+	}
+	// zlog.Info("Labelize view:", view.ObjectName(), alignment, marg)
+	viewCell = stack.Add(view, alignment, marg)
+	return
 }
