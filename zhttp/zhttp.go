caccd79f0a2928d884a715cedefd410f5236404e
Tor Langballe <tor.langballe@bridgetech.tv>
2023-04-26 12:48:34 +0200

    Typo fix

--- zhttp/zhttp.go
+++ zhttp/zhttp.go
@@ -377,7 +377,7 @@ func SendBytesSetContentLength(surl string, params Parameters) (resp *http.Respo
 	params.Headers["Content-Type"] = params.ContentType
 	// zlog.Info("SendBytesSetContentLength:", params.Method, surl)
 	if params.PrintBody {
-		zlog.Info("dum output:", surl, "\n", string(params.Body))
+		zlog.Info("dump output:", surl, "\n", string(params.Body))
 		for h, s := range params.Headers {
 			zlog.Info(h+":", s)
 		}
