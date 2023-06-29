3381cd1d82e9da9fb933ef3a068d34167b2af2d6
Tor Langballe <tor.langballe@bridgetech.tv>
2023-06-29 10:27:04 +0200

    Add RestartSelf() to zprocess

--- zprocess/zprocess!js.go
+++ zprocess/zprocess!js.go
@@ -11,6 +11,7 @@ import (
 	"runtime"
 	"sort"
 	"strings"
+	"syscall"
 
 	"github.com/mitchellh/go-ps"
 	"github.com/shirou/gopsutil/process"
@@ -248,4 +249,27 @@ func GetOpenFileCount() int {
 	}
 	count := bytes.Count([]byte(str), []byte("\n"))
 	return count - 1
+}
+
+func RestartSelf() error {
+	self, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	args := os.Args
+	env := os.Environ()
+	// Windows does not support exec syscall.
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command(self, args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+		cmd.Env = env
+		err := cmd.Run()
+		if err == nil {
+			os.Exit(0)
+		}
+		return err
+	}
+	return syscall.Exec(self, args, env)
 }
