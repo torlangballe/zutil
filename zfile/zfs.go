803c279bf8031105433c82141f344edb433ca061
Tor Langballe <tor.langballe@bridgetech.tv>
2023-09-02 14:59:14 +0200

    Add MultiFS to zfile
    It contains multiple fs.FS and acts itself as a fs.FS
    Allows Open() using any within in order from first to last in slice.
    
    Also: Added ReadBytesFromFileInFS() convenience method.

---
+++ zfile/zfs.go
@@ -0,0 +1,37 @@
+package zfile
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/fs"
+)
+
+type MultiFS []fs.FS
+
+func (m MultiFS) Open(name string) (fs.File, error) {
+	for _, f := range m {
+		file, err := f.Open(name)
+		if err == nil || !errors.Is(err, fs.ErrNotExist) {
+			return file, err
+		}
+	}
+	return nil, fs.ErrNotExist
+}
+
+func (m *MultiFS) Add(f fs.FS) {
+	*m = append(*m, f)
+}
+
+func ReadBytesFromFileInFS(f fs.FS, name string) ([]byte, error) {
+	file, err := f.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	buff := bytes.NewBuffer([]byte{})
+	_, err = io.Copy(buff, file)
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
