534a8942b2d81d01f5343f130711c71a28b7acd8
Tor Langballe <tor.langballe@bridgetech.tv>
2023-09-19 01:05:50 +0200

    Make zrpc's updatedResourcesSentToClient a zmap.LockMap

--- zrpc/zrpc_resources_server.go
+++ zrpc/zrpc_resources_server.go
@@ -3,23 +3,24 @@ package zrpc
 import (
 	"github.com/torlangballe/zutil/zhttp"
 	"github.com/torlangballe/zutil/zmap"
+	"github.com/torlangballe/zutil/zstr"
 )
 
 // This is functionality to set that a named resource has changed.
 // A RPC function can query for changed resources, and its ClientID is stored so as to
 // not report is as changed to that client until updated again.
 var (
-	updatedResourcesSentToClient = zmap.LockMap[string, map[string]bool]{}
+	updatedResourcesSentToClient zmap.LockMap[string, []string]
 )
 
 // GetUpdatedResourcesAndSetSent is called from clients (often browsers) to ask for updated resource-ids
 // The client id is stored as it having checked them out for that given update.
 func (RPCCalls) GetUpdatedResourcesAndSetSent(ci ClientInfo, int Unused, reply *[]string) error {
 	*reply = []string{}
-	updatedResourcesSentToClient.ForEach(func(res string, m map[string]bool) bool {
-		if !m[ci.ClientID] {
+	updatedResourcesSentToClient.ForEach(func(res string, c []string) bool {
+		if !zstr.StringsContain(c, ci.ClientID) {
 			*reply = append(*reply, res)
-			m[ci.ClientID] = true
+			c = append(c, ci.ClientID)
 		}
 		return true
 	})
@@ -29,12 +30,12 @@ func (RPCCalls) GetUpdatedResourcesAndSetSent(ci ClientInfo, int Unused, reply *
 // SetResourceUpdatedmarks a given resource as changed. If a client id is given, that client is NOT
 // informed the resource changed (presumably because it caused the change).
 func SetResourceUpdated(resID, byClientID string) {
-	m := map[string]bool{}
+	var c []string
 	if byClientID != "" {
-		m[byClientID] = true
+		c = []string{byClientID}
 	}
 	// fmt.Println("SetResourceUpdated:", resID, byClientID) //, "\n", zlog.GetCallingStackString())
-	updatedResourcesSentToClient.Set(resID, m)
+	updatedResourcesSentToClient.Set(resID, c)
 }
 
 // ClearResourceID clears changed status for a resource
@@ -47,12 +48,9 @@ func ClearResourceID(resID string) {
 // SetClientKnowsResourceUpdated sets that a given client now knows resource updated
 func SetClientKnowsResourceUpdated(resID, clientID string) {
 	// zlog.Info("SetClientKnowsResourceUpdated:", resID, clientID) //, "\n", zlog.GetCallingStackString())
-	m, _ := updatedResourcesSentToClient.Get(resID)
-	if m == nil {
-		m = map[string]bool{}
-	}
-	m[clientID] = true
-	updatedResourcesSentToClient.Set(resID, m)
+	c, _ := updatedResourcesSentToClient.Get(resID)
+	zstr.AddToSet(&c, clientID)
+	updatedResourcesSentToClient.Set(resID, c)
 }
 
 // SetResourceUpdatedFromClient is called from client to say it knows of update
