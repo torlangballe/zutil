64c84153a7cbdf709236ad69aa100c2db1cfe480
Tor Langballe <tor.langballe@bridgetech.tv>
2023-10-12 13:34:49 +0200

    Make zstr.TabWriter handle \r as returns in cell

--- zstr/zstabwriter.go
+++ zstr/zstabwriter.go
@@ -2,6 +2,7 @@ package zstr
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"regexp"
 	"strings"
@@ -10,12 +11,18 @@ import (
 	"github.com/torlangballe/zutil/zint"
 )
 
+// TabWriter implements an io.Writer that reads text and outputs it in columns to a writer,
+// splitting the output using \t and \n for each ine
+// TODO: Currenly uses a hack to allow multi-lines per cell using \r.
+// Need a better way, but might need to specify number of columns.
+// It can handle terminal color escape codes.
+
 type TabWriter struct {
-	MaxColumnWidth      int
-	CellDivider         string
-	RighAdjustedColumns map[int]bool
-	MaxColumnWidths     map[int]int
-	RepeatFirstRowEvery int
+	MaxColumnWidth      int          // MaxColumnWidth is a global max width for any column not otherwise specified
+	OutCellDivider      string       // OutCellDivider is what to have between columns
+	RighAdjustedColumns map[int]bool // Set RighAdjustedColumns for a zero-based index of column to right adjust
+	MaxColumnWidths     map[int]int  // MaxColumnWidths allows user to specify a max per-column
+	RepeatFirstRowEvery int          // Allows the first row (header) to be repeated every so often
 	firstLine           string
 	buffer              bytes.Buffer
 	out                 io.Writer
@@ -24,7 +31,7 @@ type TabWriter struct {
 func NewTabWriter(out io.Writer) *TabWriter {
 	t := &TabWriter{}
 	t.out = out
-	t.CellDivider = "  "
+	t.OutCellDivider = "  "
 	t.RighAdjustedColumns = map[int]bool{}
 	t.MaxColumnWidths = map[int]int{}
 	t.RepeatFirstRowEvery = 20
@@ -38,7 +45,8 @@ func (t *TabWriter) Write(b []byte) (n int, err error) {
 func (t *TabWriter) Flush() error {
 	var widths []int
 	allOutput := string(t.buffer.Bytes())
-	RangeStringLines(allOutput, true, func(sline string) bool {
+	lines := strings.Split(allOutput, "\n")
+	for _, sline := range lines {
 		sline = strings.TrimRight(sline, "\t")
 		for i, cell := range strings.Split(sline, "\t") {
 			if len(widths)-1 < i {
@@ -50,8 +58,7 @@ func (t *TabWriter) Flush() error {
 			clen := utf8.RuneCountInString(stripped)
 			zint.Maximize(&widths[i], clen)
 		}
-		return true
-	})
+	}
 	for i := range widths {
 		max := t.MaxColumnWidths[i]
 		if max == 0 {
@@ -63,7 +70,7 @@ func (t *TabWriter) Flush() error {
 	}
 	// fmt.Println("Widths:", widths)
 	var lineIndex int
-	RangeStringLines(allOutput, true, func(sline string) bool {
+	for _, sline := range lines {
 		sline = strings.TrimRight(sline, "\t")
 		if t.RepeatFirstRowEvery != 0 {
 			if lineIndex == 0 {
@@ -74,14 +81,34 @@ func (t *TabWriter) Flush() error {
 		}
 		t.outputLine(sline, widths)
 		lineIndex++
-		return true
-	})
+	}
 	return nil
 }
 
 func (t *TabWriter) outputLine(sline string, widths []int) {
+	cells := strings.Split(sline, "\t")
+	for j := 0; ; j++ {
+		var has bool
+		row := make([]string, len(cells))
+		fmt.Println("Row1:", len(row))
+		for i, c := range cells {
+			parts := strings.Split(c, "\r")
+			if len(parts) > j {
+				has = true
+				row[i] = parts[j]
+			}
+		}
+		if has {
+			t.outputCells(row, widths)
+		} else {
+			break
+		}
+	}
+}
+
+func (t *TabWriter) outputCells(cells []string, widths []int) {
 	var outLine string
-	for i, cell := range strings.Split(sline, "\t") {
+	for i, cell := range cells {
 		var cellChars, whiteChars int
 		width := widths[i]
 		shortened := ReplaceAllCapturesFunc(colorEscapeReg, cell, RegWithOutside, func(cap string, index int) string {
@@ -102,7 +129,7 @@ func (t *TabWriter) outputLine(sline string, widths []int) {
 			return cap
 		})
 		if i != 0 {
-			outLine += t.CellDivider
+			outLine += t.OutCellDivider
 		}
 		left := width - whiteChars
 		var space string
