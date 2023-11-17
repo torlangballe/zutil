cd872ca7c6ce62d4109e953f30ad72ae7f05b3ef
Tor Langballe <tor.langballe@bridgetech.tv>
2023-11-17 15:40:47 +0100

    move zdraw from zui to zutil

---
+++ zdraw/DrawUtil.go
@@ -0,0 +1,33 @@
+package zdraw
+
+import (
+	"image/color"
+
+	"github.com/torlangballe/zui/zcanvas"
+	"github.com/torlangballe/zui/zimage"
+	"github.com/torlangballe/zutil/zgeo"
+)
+
+func DrawAmountPie(rect zgeo.Rect, canvas *zcanvas.Canvas, value, strokeWidth float64, color, strokeColor zgeo.Color) {
+	path := zgeo.PathNew()
+	s := rect.Size.MinusD(strokeWidth).DividedByD(2).MinusD(1)
+	w := s.Min()
+	path.MoveTo(rect.Center())
+	path.ArcDegFromCenter(rect.Center(), zgeo.SizeBoth(w), 0, value*360)
+	canvas.SetColor(color)
+	canvas.FillPath(path)
+	line := zgeo.PathNew()
+	line.ArcDegFromCenter(rect.Center(), zgeo.SizeBoth(w), 0, 360)
+	canvas.SetColor(strokeColor)
+	canvas.StrokePath(line, strokeWidth, zgeo.PathLineRound)
+}
+
+func StrokeVertInImage(img zimage.SetableImage, x, y1, y2 int, col color.Color) {
+	clear := zgeo.ColorClear.GoColor()
+	for y := 0; y <= y1; y++ {
+		img.Set(x, y, clear)
+	}
+	for y := y1; y <= y2; y++ {
+		img.Set(x, y, col)
+	}
+}
