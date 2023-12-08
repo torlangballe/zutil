4f7c8825ac57d24e077e788514a810f60063b35f
Tor Langballe <tor.langballe@bridgetech.tv>
2023-12-08 13:00:06 +0100

    Remove strange zhttp test

--- zhttp/zhttp_test.go
+++ zhttp/zhttp_test.go
@@ -3,24 +3,20 @@
 package zhttp
 
 import (
-	"bytes"
-	"net/http"
 	"testing"
-	"time"
 )
 
 // Needs: https://github.com/agnivade/wasmbrowsertest
-// 
 func TestNewRequest(t *testing.T) {
-	start := time.Now()
-	reader := bytes.NewReader([]byte{5, 2, 2})
-	var count int
-	for time.Since(start) < time.Minute*35 {
-		count++
-		_, err := http.NewRequest("POST", "http://10.150.0.176/qtt/zrpc?method=RPCCalls.GetUpdatedResourcesAndSetSent", reader)
-		if err != nil {
-			t.Error(count, err)
-			return
-		}
-	}
+	// start := time.Now()
+	// reader := bytes.NewReader([]byte{5, 2, 2})
+	// var count int
+	// for time.Since(start) < time.Minute*35 {
+	// 	count++
+	// 	_, err := http.NewRequest("POST", "http://10.150.0.176/qtt/zrpc?method=RPCCalls.GetUpdatedResourcesAndSetSent", reader)
+	// 	if err != nil {
+	// 		t.Error(count, err)
+	// 		return
+	// 	}
+	// }
 }
