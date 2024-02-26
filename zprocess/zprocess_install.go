d76f6e08cbe73318e63952c92e81de7e0df39ee1
Tor Langballe <tor.langballe@bridgetech.tv>
2024-02-26 16:32:32 +0100

    zprocess: Big changes to Installer
    Handle linux/mac

--- zprocess/zprocess_install.go
+++ zprocess/zprocess_install.go
@@ -4,6 +4,7 @@ package zprocess
 
 import (
 	"os"
+	"os/user"
 	"strings"
 
 	"github.com/torlangballe/zutil/zfile"
@@ -15,6 +16,7 @@ type Installer struct {
 	Company     string
 	ProductName string
 	Domain      string
+	UserName    string // username to set owner of file/dirs
 }
 
 const launchAgentPlistStr = `
@@ -34,9 +36,9 @@ const launchAgentPlistStr = `
 			<integer>2048</integer>
 		</dict>
         <key>StandardOutPath</key>
-        <string>{var}/log.txt</string>
+        <string>{log}</string>
         <key>StandardErrorPath</key>
-        <string>{var}/log.txt</string>
+        <string>{log}</string>
 		<key>ProcessType</key>
 		<string>Interactive</string>
 		<key>Nice</key>
@@ -51,16 +53,53 @@ const launchAgentPlistStr = `
 	</dict>
 </plist>`
 
-func (i *Installer) OptPath(add string) string {
-	return zfile.JoinPathParts("/opt", i.Company, i.ProductName, add)
+const serviceFileStr = `
+[Unit]
+
+[Service]
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+PermissionsStartOnly=true
+ExecStart={bin} {args}
+Restart=always
+User={user}
+StandardOutput=journal
+WorkingDirectory={bindir}
+[Install]
+WantedBy=default.target
+`
+
+func (i *Installer) addPath(isFile bool, parts ...string) string {
+	var path string
+	for j, part := range parts {
+		path = zfile.JoinPathParts(path, part)
+		if j != len(parts)-1 || !isFile {
+			err := zfile.MakeDirAllIfNotExists(path)
+			zlog.AssertNotError(err, path)
+		}
+		if i.UserName != "" {
+			if zfile.Exists(path) {
+				err := zfile.SetOwnerAndMainGroup(path, i.UserName)
+				zlog.AssertNotError(err, path, i.UserName)
+			}
+		}
+	}
+	return path
 }
 
-func (i *Installer) BinPath(add string) string {
-	return zfile.JoinPathParts(i.OptPath("bin"), add)
+func (i *Installer) OptPath(isFile bool, parts ...string) string {
+	dir := zfile.JoinPathParts("/opt/", i.Company, i.ProductName)
+	parts = append([]string{dir}, parts...)
+	return i.addPath(isFile, parts...)
 }
 
-func (i *Installer) VarPath(add string) string {
-	return zfile.JoinPathParts(i.OptPath("etc/var"), add)
+func (i *Installer) BinPath(isFile bool, parts ...string) string {
+	parts = append([]string{"bin"}, parts...)
+	return i.OptPath(isFile, parts...)
+}
+
+func (i *Installer) VarPath(isFile bool, parts ...string) string {
+	parts = append([]string{"etc", "var"}, parts...)
+	return i.OptPath(isFile, parts...)
 }
 
 func (i *Installer) ID() string {
@@ -68,44 +107,77 @@ func (i *Installer) ID() string {
 }
 
 func (i *Installer) InstallProgramWithLauncher(args []string, copyBinary bool) error {
-	binDir := i.BinPath("")
-	err := zfile.MakeDirAllIfNotExists(binDir)
-	zlog.OnError(err)
-	bin := binDir + "/" + i.ProductName
+	user, _ := user.Current()
+	if user != nil {
+		i.UserName = user.Username
+	}
+	zlog.Info("InstallProgramWithLauncher1:", i.UserName)
+	if OnLinux() && i.UserName == "root" {
+		var rest string
+		wd, _ := os.Getwd()
+		if zstr.HasPrefix(wd, "/home/", &rest) {
+			i.UserName = zstr.HeadUntil(rest, "/")
+		}
+		zlog.Info("InstallProgramWithLauncher2:", i.UserName, wd)
+	}
+	zlog.Info("InstallProgramWithLauncher2:", i.UserName)
+	binDir := i.BinPath(false)
+	bin := i.BinPath(true, i.ProductName)
 	if copyBinary {
 		err := zfile.CopyFile(bin, os.Args[0])
-		if zlog.OnError(err) {
+		if zlog.OnError(err, bin, os.Args[0]) {
 			return err
 		}
 	}
-	var launchConfig, launcherPath, sargs string
-	if OnDarwin() {
-		launchConfig = launchAgentPlistStr
-		launcherPath = zfile.ExpandTildeInFilepath("~/Library/LaunchAgents/"+i.ID()) + ".plist"
-	}
-	for _, a := range args {
-		sargs += "<string>" + a + "</string>\n"
+	err := os.Chmod(bin, 0700)
+	if zlog.OnError(err, bin) {
+		return err
 	}
 
+	if i.UserName != "" {
+		err := zfile.SetOwnerAndMainGroup(bin, i.UserName)
+		if zlog.OnError(err, bin) {
+			return err
+		}
+	}
+	var launchConfig, launcherPath, serviceTool, sargs string
+	if OnDarwin() {
+		serviceTool = "launchctl"
+		launchConfig = launchAgentPlistStr
+		launcherPath = zfile.ExpandTildeInFilepath("~/Library/LaunchAgents/"+i.ID()) + ".plist"
+		for _, a := range args {
+			sargs += "<string>" + a + "</string>\n"
+		}
+	} else {
+		serviceTool = "systemctl"
+		launchConfig = serviceFileStr
+		launcherPath = "/lib/systemd/system/" + i.ID() + ".service"
+		sargs = strings.Join(args, " ")
+	}
 	replacer := strings.NewReplacer(
+		"{user}", i.UserName,
 		"{bin}", bin,
-		"{var}", i.VarPath(""),
+		"{log}", i.VarPath(true, "log.txt"),
 		"{id}", i.ID(),
 		"{bindir}", binDir,
 		"{args}", sargs,
 	)
+	zlog.Info("WriteServiceFile:", launcherPath)
 	launchConfig = replacer.Replace(launchConfig)
 	err = zfile.WriteStringToFile(launchConfig, launcherPath)
+	zlog.Info("install launcher", launcherPath, zfile.Exists(launcherPath))
 	if err != nil {
-		return zlog.Error(err, "install launcher", launcherPath)
+		return zlog.Error(err, "install launcher", launcherPath, zfile.Exists(launcherPath))
 	}
-	const launch = "launchctl"
-	_, err = RunCommand(launch, 5, "load", launcherPath)
+	// if OnLinux() && userName != "root" && userName != "" {
+	// 	zfile.SetOwnerAndMainGroup(launcherPath, userName)
+	// }
+	_, err = RunCommand(serviceTool, 5, "load", launcherPath)
 	zlog.OnError(err, launcherPath)
 	id := i.ID()
-	str, err := RunCommand(launch, 5, "stop", id)
+	str, err := RunCommand(serviceTool, 5, "stop", id)
 	zlog.OnError(err, str)
-	str, err = RunCommand(launch, 5, "start", id)
+	str, err = RunCommand(serviceTool, 5, "start", id)
 	zlog.OnError(err, str)
 	return nil
 }
