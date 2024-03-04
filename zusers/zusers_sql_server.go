f9e90a6e37a1eb21524803eefdfbcf7098a78939
Tor Langballe <tor.langballe@bridgetech.tv>
2024-03-04 10:44:45 +0100

    zsql: Remove zlog.Error for missing user
    Can happen all the time just seeing if user exists.

--- zusers/zusers_sql_server.go
+++ zusers/zusers_sql_server.go
@@ -137,7 +137,7 @@ func (s *SQLServer) GetUserIDFromToken(token string) (id int64, err error) {
 	row := s.DB.QueryRow(squery, token)
 	err = row.Scan(&id)
 	if err != nil {
-		zlog.Error(err, squery, "token:", token, zlog.CallingStackString())
+		// zlog.Error(err, squery, "token:", token, zlog.CallingStackString())
 		return 0, AuthFailedError
 	}
 	squery = "UPDATE zuser_sessions SET used=$NOW WHERE token=$1"
