7ed18a8cb3fc84b737e73a2c226a9f696de27b16
Tor Langballe <tor.langballe@bridgetech.tv>
2024-05-27 10:17:35 +0200

    Fix zstr unit test

--- zstr/zstr_test.go
+++ zstr/zstr_test.go
@@ -8,7 +8,11 @@ import (
 func TestWildCard(t *testing.T) {
 	fmt.Println("TestWildCard")
 	match := deepMatchRune([]rune("Delux Music"), []rune("*ani*"), false)
+	if match {
+		t.Error("shouldn't match")
+	}
+	match = deepMatchRune([]rune("Delux Music"), []rune("*Mu*"), false)
 	if !match {
-		t.Error("no match")
+		t.Error("shouldn't match")
 	}
 }
