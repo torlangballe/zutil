db238a44b17a37d48e6f634d525d48cc9afebba2
Tor Langballe <tor.langballe@bridgetech.tv>
2024-08-07 13:06:28 +0200

    zdesktop: Add dummy linux funcs

--- zdesktop/zdesktop_linux.go
+++ zdesktop/zdesktop_linux.go
@@ -1,6 +1,8 @@
 package zdesktop
 
 import (
+	"image"
+
 	"github.com/torlangballe/zutil/zdevice"
 	"github.com/torlangballe/zutil/zgeo"
 )
@@ -20,7 +22,22 @@ func GetAppNameOfBrowser(btype zdevice.BrowserType, fullName bool) string {
 	return ""
 }
 
-func CloseOldWindowWithSamePIDAndRect(pid int64, r zgeo.Rect) {}
-func CloseOldWindowWithSamePIDAndRectOnceNew(pid int64, r zgeo.Rect) bool {
-	return true
+func CloseOldWindowWithSamePIDAndRect(pid int64, r zgeo.Rect)                    {}
+func CloseOldWindowWithSamePIDAndRectOnceNew(pid int64, r zgeo.Rect) bool        { return true }
+func OpenURLInBrowser(surl string, btype zdevice.BrowserType, args ...any) error { return nil }
+func SendQuitCommandToApp(app string) error                                      { return nil }
+func PrintWindowTitles()                                                         {}
+func CloseWindowForTitle(title, app string) error                                { return nil }
+func CanGetWindowInfo() bool                                                     { return true }
+func CanRecordScreen() bool                                                      { return true }
+func CanControlComputer(prompt bool) bool                                        { return true }
+func GetAllWindowTitlesForApp(app string) []string                               { return nil }
+func ShowAlert(str string)                                                       {}
+
+func SetWindowRectForTitle(title, app string, rect zgeo.Rect) (winPID int64, err error) {
+	return 0, nil
+}
+
+func GetImageForWindowTitle(title, app string, oldPID int64, insetRect zgeo.Rect) (img image.Image, pid int64, err error) {
+	return nil, 0, nil
 }
