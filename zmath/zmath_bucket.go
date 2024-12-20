7a8e73773098061bf4b47be8eb3cbefb10b33b3e
Tor Langballe <tor.langballe@bridgetech.tv>
2024-12-20 11:05:26 +0100

    zmath: New BucketResult struct
    Used in BucketFilter, stores min/max too

--- zmath/zmath_bucket.go
+++ zmath/zmath_bucket.go
@@ -10,22 +10,29 @@ type BucketType string
 
 const (
 	BucketNearest BucketType = "nearest"
+	BucketMax     BucketType = "max"
 )
+
+type BucketResult struct {
+	CurrentCellPos float64
+	BestVal        float64
+	BestPos        float64
+	BestPayload    any
+	LastPayload    any
+	MaxVal         float64
+	MinVal         float64
+}
 
 // BucketFilter accepts pos+values, aggregating all that are within a repeating pos period
 // It assumes the positions are coming in order.
 // With a BucketNearest type, it aggregates on the pos nearest the middle of the period, storing it's value too.
 type BucketFilter struct {
+	BucketResult
 	Type    BucketType
-	GotFunc func(payload, lastPayload any, pos, periodPos, val float64, periodIndex int)
+	GotFunc func(result BucketResult, periodIndex int)
 
-	period         float64
-	startPos       float64
-	currentCellPos float64
-	bestVal        float64
-	bestPos        float64
-	bestPayload    any
-	lastPayload    any
+	period   float64
+	startPos float64
 }
 
 func NewBucketFilter(start, period float64) *BucketFilter {
@@ -34,52 +41,60 @@ func NewBucketFilter(start, period float64) *BucketFilter {
 	f.startPos = start
 	// zlog.Info("NewBucket:", zlog.Pointer(f), start, period)
 	f.period = period
-	f.currentCellPos = start
-	f.bestPayload = nil
+	f.CurrentCellPos = start
+	f.BestPayload = nil
 	return f
 }
 
 func (f *BucketFilter) Flush() {
-	if f.bestPayload != nil {
-		periodIndex := int((f.currentCellPos - f.startPos) / f.period)
-		// zlog.Info("Flush:", zlog.Pointer(f), periodIndex, f.bestPayload != nil, "current:", time.UnixMicro(int64(f.currentCellPos)), "best:", time.UnixMicro(int64(f.bestPos)))
-		f.GotFunc(f.bestPayload, f.lastPayload, f.bestPos, f.currentCellPos, f.bestVal, periodIndex)
-		f.bestPayload = nil
-		f.lastPayload = nil
+	if f.BestPayload != nil {
+		periodIndex := int((f.CurrentCellPos - f.startPos) / f.period)
+		// zlog.Info("Flush:", zlog.Pointer(f), periodIndex, f.BestPayload != nil, "current:", time.UnixMicro(int64(f.CurrentCellPos)), "best:", time.UnixMicro(int64(f.bestPos)))
+		f.GotFunc(f.BucketResult, periodIndex)
+		f.BestPayload = nil
+		f.LastPayload = nil
 	} else {
 		// zlog.Info("NoFlush:", zlog.Pointer(f))
 	}
 }
 
 func (f *BucketFilter) aggregate(payload any, pos, val float64) {
-	f.lastPayload = payload
-	// zlog.Info("aggregate1:", zlog.Pointer(f), time.UnixMicro(int64(pos)), f.bestPayload != nil)
-	if f.bestPayload == nil {
-		f.bestPayload = payload
-		f.bestPos = pos
-		f.bestVal = val
-		f.currentCellPos = f.startPos + RoundToModF64(pos-f.startPos, f.period)
-		// zlog.Info("aggregate new:", f.startPos, zlog.Pointer(f), time.UnixMicro((int64(pos))), "current:", time.UnixMicro((int64(f.currentCellPos))), time.Duration(pos-f.currentCellPos), time.Duration(f.period)*time.Microsecond)
+	f.LastPayload = payload
+	// zlog.Info("aggregate1:", zlog.Pointer(f), time.UnixMicro(int64(pos)), f.BestPayload != nil)
+	if f.BestPayload == nil {
+		f.MinVal = val
+		f.MaxVal = val
+		f.BestPayload = payload
+		f.BestPos = pos
+		f.BestVal = val
+		f.CurrentCellPos = f.startPos + RoundToModF64(pos-f.startPos, f.period)
+		// zlog.Info("aggregate new:", f.startPos, zlog.Pointer(f), time.UnixMicro((int64(pos))), "current:", time.UnixMicro((int64(f.currentCellPos))), time.Duration(pos-f.CurrentCellPos), time.Duration(f.period)*time.Microsecond)
 		return
 	}
+	f.MinVal = min(f.MinVal, val)
+	f.MaxVal = min(f.MaxVal, val)
+	var add bool
 	switch f.Type {
+	case BucketMax:
+		add = (val > f.MaxVal)
 	case BucketNearest:
-		mid := f.currentCellPos + f.period/2
-		if math.Abs(f.bestPos-mid) > math.Abs(pos-mid) {
-			f.bestPos = pos
-			f.bestVal = val
-			f.bestPayload = payload
-		}
+		mid := f.CurrentCellPos + f.period/2
+		add = (math.Abs(f.BestPos-mid) > math.Abs(pos-mid))
+	}
+	if add {
+		f.BestPos = pos
+		f.BestVal = val
+		f.BestPayload = payload
 	}
 }
 
 func (f *BucketFilter) Set(payload any, pos, val float64) {
 	// zlog.Info("buck.Set:", zlog.Pointer(f), time.UnixMicro(int64(pos)), val)
-	if pos < f.currentCellPos {
-		zlog.Error("val before start:", payload, pos, f.currentCellPos)
+	if pos < f.CurrentCellPos {
+		zlog.Error("val before start:", payload, pos, f.CurrentCellPos)
 		return
 	}
-	if pos >= f.currentCellPos+f.period {
+	if pos >= f.CurrentCellPos+f.period {
 		// zlog.Info("buck.Flush from set:", zlog.Pointer(f), pos)
 		f.Flush()
 	}
