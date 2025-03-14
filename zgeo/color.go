edd0aab7b1e2775ac0d15bda6e390177c28d9332
Tor Langballe <tor.langballe@bridgetech.tv>
2025-03-14 11:07:34 +0100

    zgeo.Color: Tweak Pink color

--- zgeo/color.go
+++ zgeo/color.go
@@ -476,7 +476,7 @@ var ColorBlue = ColorNew(0, 0, 1, 1)
 var ColorNavy = ColorNew(0, 0, 0.5, 1)
 var ColorFuchsia = ColorNew(1, 0, 1, 1)
 var ColorPurple = ColorNew(0.5, 0, 0.5, 1)
-var ColorPink = ColorNew(1, 0.8, 0.8, 1)
+var ColorPink = ColorNew(1, 0.2, 1, 1)
 
 var ColorDistinctList = []Color{ColorOrange, ColorCyan, ColorMagenta, ColorGray, ColorBlack, ColorRed, ColorMaroon, ColorYellow, ColorOlive, ColorLime, ColorGreen, ColorDarkGreen, ColorAqua, ColorTeal, ColorBlue, ColorNavy, ColorDarkGray, ColorFuchsia, ColorPurple, ColorLightGray}
 
