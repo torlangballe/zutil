6e1a850a34ee17f5d5da4ac7e63dce51bc1192f8
Tor Langballe <tor.langballe@bridgetech.tv>
2025-08-07 15:40:22 +0200

    zdevice: Use sysctl on mac after all

--- zdevice/zdevice_unix.go
+++ zdevice/zdevice_unix.go
@@ -5,11 +5,14 @@ package zdevice
 import (
 	"errors"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/shirou/gopsutil/v4/net"
 	"github.com/torlangballe/zutil/zlog"
 	"github.com/torlangballe/zutil/zprocess"
+	"github.com/torlangballe/zutil/zstr"
 	"github.com/torlangballe/zutil/ztimer"
 )
 
@@ -117,7 +120,18 @@ func HardwareTypeAndVersion() (string, float32) {
 	case "linux":
 		return "Linux Server", 1
 	case "darwin":
-		return "Apple Computer", 1
+		str, err := zprocess.RunCommand("sysctl", 0, "-n", "hw.model")
+		if err != nil {
+			zlog.Error("run", err)
+			return "", 0
+		}
+		i := strings.IndexAny(str, zstr.Digits)
+		if i == -1 {
+			return str, 1
+		}
+		name := zstr.Head(str, i)
+		num, _ := strconv.ParseFloat(zstr.Body(str, i, -1), 32)
+		return name, float32(num)
 	}
 	return "<unknown>", -1
 }
