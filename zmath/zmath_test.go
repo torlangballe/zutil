25baecaf91ddc5ae96512f41ce584f1588508693
Tor Langballe <tor.langballe@bridgetech.tv>
2026-04-01 10:29:46 +0200

    Rename zmath_tree_test to zmath_test

--- zmath/zmath_test.go
+++ zmath/zmath_test.go
@@ -7,8 +7,22 @@ import (
 	"github.com/torlangballe/zutil/ztesting"
 )
 
+type Hierarchy struct {
+	ID       string
+	ParentID string
+	Instance string
+}
+
+func (h Hierarchy) Identifier() string {
+	return h.ID
+}
+
+func (h Hierarchy) ParentIdentifier() string {
+	return h.ParentID
+}
+
 func TestTree(t *testing.T) {
-	rows := []Hierary{
+	rows := []Hierarchy{
 		{ID: "Animal", ParentID: "", Instance: ""},
 		{ID: "Mammal", ParentID: "Animal", Instance: ""},
 		{ID: "Bird", ParentID: "Animal", Instance: ""},
@@ -21,4 +35,10 @@ func TestTree(t *testing.T) {
 	tree := MakeTree(rows)
 	str := fmt.Sprint(tree)
 	ztesting.Equal(t, str, "[{Animal 1 } {Mammal 2 } {Dog 3 } {Cat 3 } {TomCat 4 } {Monkey 3 } {Chimpanzee 4 } {Bird 2 }]")
+}
+
+func TestEaseInOut(t *testing.T) {
+	ztesting.NearEqualF(t, EaseInOut(0), 0.0, "EaseInOut")
+	ztesting.NearEqualF(t, EaseInOut(0.5), 0.5, "EaseInOut")
+	ztesting.NearEqualF(t, EaseInOut(1), 1.0, "EaseInOut")
 }
