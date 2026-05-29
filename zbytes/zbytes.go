5f1b39b036219bd330e90ac09394ce7c11c0b39a
Tor Langballe <tor.langballe@bridgetech.tv>
2026-05-29 11:14:31 +0200

    zbytes: Add RollingBuffer

--- zbytes/zbytes.go
+++ zbytes/zbytes.go
@@ -13,6 +13,16 @@ import (
 	"golang.org/x/text/transform"
 )
 
+// RollingBuffer is a buffer that keeps only the last N bytes of data written to it.
+// It implements io.Writer, io.Reader and io.Seeker interfaces.
+// It can be used for keeping logs in memory with a maximum size.
+// It never returns EOF on read, but returns 0, nil when there is no data to read. Yet.
+type RollingBuffer struct {
+	data           []byte
+	currentReadPos int
+	maxSize        int
+}
+
 const (
 	KiloByte  = 1024
 	MegaByte  = 1024 * KiloByte
@@ -131,3 +141,58 @@ func CalculateCRC32(data []byte, seed int64) int64 {
 	io.Copy(crc, buf)
 	return int64(crc.Sum32())
 }
+
+func NewRollingBuffer(maxSize int) *RollingBuffer {
+	return &RollingBuffer{
+		maxSize: maxSize,
+	}
+}
+
+func (r *RollingBuffer) Write(p []byte) (n int, err error) {
+	r.data = append(r.data, p...)
+	diff := len(r.data) - r.maxSize
+	if diff > 0 {
+		r.data = r.data[diff:]
+		if r.currentReadPos > diff {
+			r.currentReadPos -= diff
+		} else {
+			r.currentReadPos = 0
+		}
+	}
+	return len(p), nil
+}
+
+func (r *RollingBuffer) Seek(offset int64, whence int) (int64, error) {
+	dlen := len(r.data)
+	switch whence {
+	case io.SeekStart:
+		if offset < 0 || offset > int64(dlen) {
+			return 0, io.ErrUnexpectedEOF
+		}
+		r.currentReadPos = int(offset)
+	case io.SeekCurrent:
+		if r.currentReadPos+int(offset) < 0 || r.currentReadPos+int(offset) > int(dlen) {
+			return 0, io.ErrUnexpectedEOF
+		}
+		r.currentReadPos += int(offset)
+	case io.SeekEnd:
+		if int64(dlen)-offset < 0 || int64(dlen)-offset > int64(dlen) {
+			return 0, io.ErrUnexpectedEOF
+		}
+		r.currentReadPos = dlen - int(offset)
+	}
+	return int64(r.currentReadPos), nil
+}
+
+func (r *RollingBuffer) Read(p []byte) (n int, err error) {
+	dlen := len(r.data)
+	if dlen == 0 || r.currentReadPos >= dlen {
+		return 0, nil
+	}
+	diff := dlen - r.currentReadPos
+	get := min(diff, len(p))
+	copy(p, r.data[r.currentReadPos:r.currentReadPos+get])
+	r.currentReadPos += get
+	return int(get), nil
+}
+
